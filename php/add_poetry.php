29190f687fe52c2451e7d961ed137d271a1c629d
abhinav3254 <abhinav3254@gmail.com>
2022-04-30 22:43:54 +0530

    API Development

--- php/add_poetry.php
+++ php/add_poetry.php
@@ -26,4 +26,21 @@ $POETRY = $_POST['poetry'];
 $POET_NAME = $_POST['poet_name'];
 
 
+// ***********************Making Request **************************
+
+$query = "INSERT INTO poetry(poetry_data,poet_name) VALUES('$POETRY','$POET_NAME')";
+
+$result = $conn -> query($query);
+
+if($result == 1) {
+    $responce = array("status" => "1","message" => "poetry succesfully inserted");
+} else {
+    $responce = array("status" => "0","message" => "poetry not succesfully inserted");
+}
+
+// In php we use echo to print anything
+
+echo json_encode($responce);
+
+
 ?>
